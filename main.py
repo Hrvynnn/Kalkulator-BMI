55e7bb4f9e69a0bd13b8dbb8f052a523e28eb838
Hrvynnn <kakhervian@gmail.com>
2026-04-29 20:32:04 +0700

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ from google import genai
 # ==========================================
 # KONFIGURASI API GEMINI
 # ==========================================
-API_KEY = genai.Client(api_key=st.secrets["GEMINI_API_KEY"])  
+API_KEY = genai.Client(api_key=st.secrets["GEMINI_API_KEY"])
+client = genai.Client(api_key=API_KEY)
 # Mengatur konfigurasi halaman
 st.set_page_config(page_title="Kalkulator BMI", page_icon="⚖️", layout="centered")
 
